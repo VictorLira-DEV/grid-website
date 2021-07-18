4abe8387a200ebd67dccab6d1a0f49c8b58e1c34
VictorLira <victorliradev@gmail.com>
2021-07-18 15:57:04 -0300

    replacing CSS with Sass

--- contact/main.js
+++ contact/main.js
@@ -7,85 +7,85 @@ const password2 = document.getElementById("password2");
 const cellphone = document.getElementById("cellphone");
 
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  chechInputs();
+    chechInputs();
 });
 
 function chechInputs() {
-  const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-  const email2Value = email2.value.trim();
-  const passwordValue = password.value.trim();
-  const password2Value = password2.value.trim();
-  const cellphoneValue = Number(cellphone.value);
-  //username
-  if (usernameValue === "") {
-    setErrorFor(username, "Username cannot be blank");
-  } else {
-    setSuccessFor(username);
-  }
-  //email
-  if (emailValue === "") {
-    setErrorFor(email, "E-mail cannot be blank");
-  } else if (!isEmail(email.value)) {
-    setErrorFor(email, "E-mail is not valid");
-  } else {
-    setSuccessFor(email);
-  }
-  //email2
-  if (email2Value !== emailValue || email2Value == "") {
-    setErrorFor(email2, "E-mail cannot be blank");
-  } else {
-    setSuccessFor(email2);
-  }
-  //password
-  if (passwordValue === "") {
-    setErrorFor(password, "Password cannot be blank");
-  } else {
-    setSuccessFor(password);
-  }
-  //password2
-  if (passwordValue === "" || passwordValue !== password2Value) {
-    setErrorFor(password2, "This password is not valid");
-  } else {
-    setSuccessFor(password2);
-  }
-  //cellphone
-  if (cellphoneValue === 0) {
-    setErrorFor(cellphone, "cellphone cannot be blank");
-  } else if (!isNumeric(cellphoneValue)) {
-    setErrorFor(cellphone, "cellphone cannot be blank");
-  } else {
-    setSuccessFor(cellphone);
-    console.log(cellphoneValue);
-  }
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const email2Value = email2.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    const cellphoneValue = Number(cellphone.value);
+    //username
+    if (usernameValue === "") {
+        setErrorFor(username, "Username cannot be blank");
+    } else {
+        setSuccessFor(username);
+    }
+    //email
+    if (emailValue === "") {
+        setErrorFor(email, "E-mail cannot be blank");
+    } else if (!isEmail(email.value)) {
+        setErrorFor(email, "E-mail is not valid");
+    } else {
+        setSuccessFor(email);
+    }
+    //email2
+    if (email2Value !== emailValue || email2Value == "") {
+        setErrorFor(email2, "E-mail cannot be blank");
+    } else {
+        setSuccessFor(email2);
+    }
+    //password
+    if (passwordValue === "") {
+        setErrorFor(password, "Password cannot be blank");
+    } else {
+        setSuccessFor(password);
+    }
+    //password2
+    if (passwordValue === "" || passwordValue !== password2Value) {
+        setErrorFor(password2, "This password is not valid");
+    } else {
+        setSuccessFor(password2);
+    }
+    //cellphone
+    if (cellphoneValue === 0) {
+        setErrorFor(cellphone, "cellphone cannot be blank");
+    } else if (!isNumeric(cellphoneValue)) {
+        setErrorFor(cellphone, "cellphone cannot be blank");
+    } else {
+        setSuccessFor(cellphone);
+        console.log(cellphoneValue);
+    }
 }
 
 function isNumeric(num) {
-  return !isNaN(num);
+    return !isNaN(num);
 }
 
 function setErrorFor(input, message) {
-  const formInside = input.parentElement;
-  const small = formInside.querySelector("small");
-  small.textContent = message;
+    const formInside = input.parentElement;
+    const small = formInside.querySelector("small");
+    small.textContent = message;
 
-  input.parentElement.classList.remove("success");
-  input.parentElement.classList.add("error");
+    input.parentElement.classList.remove("success");
+    input.parentElement.classList.add("error");
 }
 
 function setSuccessFor(input) {
-  const formInside = input.parentElement;
-  const small = formInside.querySelector("small");
-  small.textContent = "";
+    const formInside = input.parentElement;
+    const small = formInside.querySelector("small");
+    small.textContent = "";
 
-  input.parentElement.classList.remove("error");
-  input.parentElement.classList.add("success");
+    input.parentElement.classList.remove("error");
+    input.parentElement.classList.add("success");
 }
 
 function isEmail(email) {
-  return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
+    return /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+    );
 }
--- services/main.js
+++ services/main.js
@@ -8,44 +8,44 @@ const closeBtn = document.querySelector(".close-btn");
 const popUp = document.querySelector(".pop-up");
 
 picture1.addEventListener("click", function () {
-  setBackGroundImage("src", "./images/picture-1-original.jpeg");
-  removeHiddenClass();
+    setBackGroundImage("src", "./images/picture-1-original.jpeg");
+    removeHiddenClass();
 });
 
 picture2.addEventListener("click", function () {
-  setBackGroundImage("src", "./images/picture-2-original.jpg");
-  removeHiddenClass();
+    setBackGroundImage("src", "./images/picture-2-original.jpg");
+    removeHiddenClass();
 });
 
 picture3.addEventListener("click", function () {
-  setBackGroundImage("src", "./images/picture-3-original.jpg");
-  removeHiddenClass();
+    setBackGroundImage("src", "./images/picture-3-original.jpg");
+    removeHiddenClass();
 });
 
 picture4.addEventListener("click", function () {
-  setBackGroundImage("src", "./images/picture-4-original.jpg");
-  removeHiddenClass();
+    setBackGroundImage("src", "./images/picture-4-original.jpg");
+    removeHiddenClass();
 });
 
 overlayExterna.addEventListener("click", exitPicture);
 closeBtn.addEventListener("click", exitPicture);
 
 function removeHiddenClass() {
-  overlayExterna.classList.remove("hidden");
-  popUp.classList.remove("hidden");
+    overlayExterna.classList.remove("hidden");
+    popUp.classList.remove("hidden");
 }
 
 function exitPicture() {
-  overlayExterna.classList.add("hidden");
-  popUp.classList.add("hidden");
+    overlayExterna.classList.add("hidden");
+    popUp.classList.add("hidden");
 }
 
 let img = document.createElement("img");
 
 function setBackGroundImage(src, jpeg) {
-  img.setAttribute(src, jpeg);
-  img.setAttribute("width", "100%");
-  img.setAttribute("height", "100%");
+    img.setAttribute(src, jpeg);
+    img.setAttribute("width", "100%");
+    img.setAttribute("height", "100%");
 
-  popUp.appendChild(img);
+    popUp.appendChild(img);
 }
